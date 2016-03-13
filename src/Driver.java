3b5d73457f460c51bb63fc185e13cb566b882a0f
Salvatore Ficara <Salvatore Ficara>
2016-03-13 17:51:17 -0400

    Nearly complete adt oopda lab

--- src/Driver.java
+++ src/Driver.java
@@ -2,19 +2,23 @@
 public class Driver {
 
 	public static void main(String[] args){
-		HexNumber numOne = new HexNumber("7");
-		HexNumber numTwo = new HexNumber("FFFFFFFFFFFFFFF6");
-		HexNumber numThree = new HexNumber(10);
-		HexNumber numFour = new HexNumber(-10);
-		
+		HexNumber numOne = new HexNumber("0a");
+//		HexNumber numTwo = new HexNumber("5");
+//		HexNumber numThree = new HexNumber(10);
+//		HexNumber numFour = new HexNumber(-11);
+//		
 		System.out.println("Num One : Hex Value = " + numOne.getHexValue() + " Decimal Value : " + numOne.getDecimalValue());
-		System.out.println("Num two : Hex Value = " + numTwo.getHexValue() + " Decimal Value : " + numTwo.getDecimalValue());
-		System.out.println("Num three : Hex Value = " + numThree.getHexValue() + " Decimal Value : " + numThree.getDecimalValue());
-		System.out.println("Num four : Hex Value = " + numFour.getHexValue() + " Decimal Value : " + numFour.getDecimalValue());
-		numOne.add(numTwo);
-		System.out.println("Num one + Num two = " + numOne.getDecimalValue() + " , "  + numOne.getHexValue());
+//		System.out.println("Num two : Hex Value = " + numTwo.getHexValue() + " Decimal Value : " + numTwo.getDecimalValue());
+//		System.out.println("Num three : Hex Value = " + numThree.getHexValue() + " Decimal Value : " + numThree.getDecimalValue());
+//		System.out.println("Num four : Hex Value = " + numFour.getHexValue() + " Decimal Value : " + numFour.getDecimalValue());
+//		numOne.add(numTwo);
+//		System.out.println("Num one + Num two = " + numOne.getDecimalValue() + " , "  + numOne.getHexValue());
+//		numOne.multiply(numTwo);
+//		numOne.divide(numTwo);
+//		System.out.println("Num one + Num two = " + numOne.getDecimalValue() + " , "  + numOne.getHexValue());
 
-		
+
+		System.out.println(HexNumber.isHex("1DDFAap"));
 		
 	}	
 }
--- src/HexNumber.java
+++ src/HexNumber.java
@@ -1,12 +1,17 @@
+import java.util.Iterator;
+
+import java.text.*;
+
 
 public class HexNumber {
 	private static final long HEX_BASE = 16;
 	private static final long BINARY_BASE = 2;
 	private static final int BIT_NUMBER = 64;
 	private static final String HEX_SYMBOLS = "10A11B12C13D14E15F"; 
-	private static final String HEX_INDEX ="ABCDEF";
-	private static final int[] values = new int[]{10,11,12,13,14,15};
+	private static final String HEX_INDEX ="ABCDEFabcdef";
+	private static final int[] values = new int[]{10,11,12,13,14,15,10,11,12,13,14,15};
 	private static final String BINARY_REGEX = "[(01)]|[(10)]+";
+	private static final String HEX_VERIFY = "[0-9a-fA-F]";
 	private String hexString;
 	private long hexDecimalValue;
 
@@ -21,8 +26,6 @@ public class HexNumber {
 		hexString = hexValue(decimalValue);
 	}
 
-
-
 	public long getDecimalValue(){
 		return hexDecimalValue;
 	}
@@ -35,15 +38,40 @@ public class HexNumber {
 	private void setHexString(String hexVal){
 		hexString = hexVal;
 	}
-	
+
 	private void setDecimalValue(long decimalVal){
 		hexDecimalValue = decimalVal;
 	}
+	
+	//Add two hex numbers
 	public void add(HexNumber hexObject){
 		hexDecimalValue += hexObject.getDecimalValue();
 		hexString = hexValue(hexDecimalValue);
 	}
 
+	//Multiply two hex numbers
+	public void multiply(HexNumber hexObject){
+		hexDecimalValue = hexDecimalValue * hexObject.getDecimalValue();
+		hexString = hexValue(hexDecimalValue);
+	}
+	
+	//Divide two hex numbers
+	public void divide(HexNumber hexObject){
+		hexDecimalValue = hexDecimalValue / hexObject.getDecimalValue();
+		hexString = hexValue(hexDecimalValue);
+	}
+	
+	//Substract two hex numbers
+	public void subtract(HexNumber hexObject){
+		hexDecimalValue = hexDecimalValue - hexObject.getDecimalValue();
+		hexString = hexValue(hexDecimalValue);
+	}
+	
+	//To string
+	public String toString(){
+		return hexString;
+	}
+	
 	//convert a decimal value to a hex string
 	public String hexValue(long decimal){
 		String hexValue = "";
@@ -58,12 +86,80 @@ public class HexNumber {
 		return hexValue;
 	}
 
+	//Verify is input is a valid hex value
+	public static boolean isHex(String hex){
+		StringCharacterIterator s = new StringCharacterIterator(hex);
+		Character currentChar=s.first();
+					
+		for(int i = 0; i<hex.length(); i++ ){		
+			if(String.valueOf(currentChar).matches(HEX_VERIFY)){
+			}
+			else{
+				return false;
+			}
+			currentChar = s.next();
+		}
+		return true;
+	}
+	
 	//Convert a hex string to a decimal value
 	public long decimalValue(String hex){
-		return convertHexStringToDecimal(hex);
+		long val = 0;
+		if(Integer.valueOf(hexValueGetter(hex.substring(0, 1)))>=8){
+			if(hex.length()<16){
+				hex = paddingHexString(hex);
+				setHexString(hex);
+			}
+			String hexToBin = convertHexToBinary(hex);
+			
+			StringBuffer reverseBits = reverseBits(hexToBin);
+			String negativeBinary = twosComplement(reverseBits);
+			//System.out.println(negativeBinary);
+			val = binaryArithmetic(negativeBinary);
+
+		}
+		else{
+			if(hex.length()<16){
+				hex = paddingHexString(hex);
+				setHexString(hex);
+			}
+			val = convertHexStringToDecimal(hex);
+		}
+		//System.out.println(hex);
+		return val;
 	}
 
-
+	//Hex padding
+	private String paddingHexString(String hex){
+		String paddingValue = "0";
+		if(Integer.valueOf(hexValueGetter(hex.substring(0, 1)))>=8){
+			paddingValue = "F";
+		}
+			for(int i = 0; hex.length()!=16; i++){
+				hex = paddingValue + hex;
+			}		
+		return hex;
+	}
+	
+	//Perform binary arithmetic
+	private long binaryArithmetic(String bin){
+		long total = 0;
+		String currentChar;
+		//int lastOccurenceOfOne = bin.lastIndexOf("1");
+		int start = bin.length() - 1;
+		int next = bin.length();
+		for(int i = 0; i<bin.length(); i++){
+			currentChar = bin.substring(start, next);
+			
+			if(hexValueGetter(currentChar)==1){
+				
+				total += Math.pow(BINARY_BASE, i);
+			}
+			start--;
+			next--;
+		}
+		return total * -1;
+	}
 
 	//Converts a positive decimal value to binary
 	private String positiveDecimalToBinary(long decimal, int bits){
@@ -168,13 +264,45 @@ public class HexNumber {
 
 	//Not used but may be useful in the future
 	public static String fourBitBinary(int val){
+		
 		String binary = "";
 		String currentBit ="";
 		while(binary.length()<4){
-			binary  += val%2;
+			binary = val%2 + binary;
 			val /= 2;
 		}
+		
 		return binary;
+	}
+
+	//Convert hex to binary
+	public static String convertHexToBinary(String hex){
+		String binary = "";
+		String currentChar;
+		int start = hex.length()-1;
+		int next = hex.length();
+		while(binary.length()<64){
+			currentChar = hex.substring(start, next);			
+			
+			int currentVal = hexValueGetter(currentChar);
+			binary = fourBitBinary(currentVal) + binary;
+
+			start--;
+			next--;
+		}
+		return binary;
+	}
+
+	//Hex value getter
+	public static int hexValueGetter(String hexChar){
+		int hexVal = 0;
+		if(HEX_INDEX.contains(hexChar)){
+			hexVal = values[HEX_INDEX.indexOf(hexChar)];
+		}
+		else{
+			hexVal = Integer.valueOf(hexChar);	
+		}	
+		return hexVal;
 	}
 
 	//Convert a signed hex string to a decimal
@@ -189,18 +317,13 @@ public class HexNumber {
 
 		for(int i = 0; i<hexLength; i++){
 			currentHexChar = hex.substring(startIndex, nextIndex);
-			if(HEX_INDEX.contains(currentHexChar)){
-				currentHexValue = values[HEX_INDEX.indexOf(currentHexChar)];
-				if(i==hexLength-1){
-					leftMostHex = currentHexValue;
-				}
+
+			currentHexValue = hexValueGetter(currentHexChar);
+			if(i==hexLength-1){
+				leftMostHex = currentHexValue;
 			}
-			else{
-				currentHexValue = Integer.valueOf(currentHexChar);
-				if(i==hexLength-1){
-					leftMostHex = currentHexValue;
-				}
-			}
+
+
 			total += currentHexValue * (long)Math.pow(HEX_BASE, i);
 			startIndex--;
 			nextIndex--;
